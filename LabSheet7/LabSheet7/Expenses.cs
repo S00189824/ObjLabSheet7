de6565f0bf116b7dc83a25923229b35f55bb6f21
David Coleman <S00189824@mail.itsligo.ie>
2019-11-14 21:55:25 +0000

    Ex1 finished

---
+++ LabSheet7/LabSheet7/Expenses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet7
+{
+    public class Expenses
+
+    {
+        public static Random rng = new Random();
+        public static string[] categories = new string[] { "travel ", "Entertainment", "Office" };
+
+        private int _Amount;
+
+        public decimal _Cost
+        {
+            get { return Amount; }
+            set
+            {
+
+                _Amount = value;
+            }
+        }
+
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        //public decimal Cost { get; set; }
+
+        public static decimal totalexpense;
+
+        public Expenses(string category, decimal amount,DateTime date)
+        {
+            Category = category;
+            _Cost = amount;
+            Date = date;
+
+            totalexpense += amount;
+        }
+
+        //Method
+        public override string ToString()
+        {
+            return $"{Category} {_Cost:C} on {Date.ToShortDateString()}";
+        }
+
+        public static Expenses GerneratorRandomExpense()
+        {
+            return new Expenses(categories[rng.Next(0, categories.Length)], rng.Next(1, 100), new DateTime(2019, 11, rng.Next(1, 30)));
+        }
+    }
+}
--- LabSheet7/LabSheet7/MainWindow.xaml.cs
+++ LabSheet7/LabSheet7/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,35 @@ namespace LabSheet7
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Expenses> expense;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Expenses ex1 = new Expenses("travel",30.99m, new DateTime(2019, 09, 10));
+            //Expenses ex2 = new Expenses( "Entertainment",64.99m, new DateTime(2019, 01, 18));
+            //Expenses ex3 = new Expenses("Office", 40.99m,new DateTime(2019, 11, 22));
+
+            //List<Expenses> expenses = new List<Expenses>();
+            expense = new ObservableCollection<Expenses>();
+            expense.Add(Expenses.GerneratorRandomExpense());
+            expense.Add(Expenses.GerneratorRandomExpense());
+            expense.Add(Expenses.GerneratorRandomExpense());
+
+            _txListExpenses.ItemsSource = expense;
+        }
+
+        private void txbtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            expense.Add(Expenses.GerneratorRandomExpense());
+
+            txblkTotal.Text = Expenses.totalexpense.ToString();
+
         }
     }
 }
